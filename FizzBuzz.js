5b2a6f6e5e04a41eace817e44865b87fddd9411c
AlexanderBB2301 <114573007+AlexanderBB2301@users.noreply.github.com>
2022-10-14 11:24:21 +0100

    Add files via upload

---
+++ FizzBuzz.js
@@ -0,0 +1,11 @@
+for (var i = 1; i < 20; i++) {
+  if (i % 3 === 0 && i % 5 !== 0) {
+    console.log("FIZZ");
+  } else if (i % 5 === 0 && i % 3 !== 0) {
+    console.log("BUZZ");
+  } else if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FIZZBUZZ");
+  } else {
+    console.log(i);
+  }
+}
